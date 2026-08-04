5b3da1e2dccacf2c09501b47804514d08da53c17
Tarun <gtarun.cs25@bmsce.ac.in>
2026-08-04 16:46:21 +0000

    Sync LeetCode submission Runtime - 2 ms (99.30%), Memory - 45.2 MB (35.67%)

--- 0125-valid-palindrome/solution.java
+++ 0125-valid-palindrome/solution.java
@@ -1,20 +1,34 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String rev="";
-        String new1 ="";
-        for (int i =0,j=s.length()-1;i<s.length() && j>=0;i++,j--)
+       boolean x=true;
+       int i=0,j=s.length()-1;
+       while(i<=j){
+       char I=s.charAt(i);
+       char J=s.charAt(j);
+       
+        if ((s.charAt(i)>'Z'|| s.charAt(i)<'A') && (s.charAt(i)>'z'|| s.charAt(i)<'a')&&(s.charAt(i)<'0'|| s.charAt(i)>'9'))
         {
-            if(s.charAt(i)>='a' && s.charAt(i)<='z' || s.charAt(i)>='0'&& s.charAt(i)<='9')
-             new1 += s.charAt(i);
-             else  if(s.charAt(i)>='A' && s.charAt(i)<='Z')
-             new1 += (char)((int)s.charAt(i)+32);
-
-             if(s.charAt(j)>='a' && s.charAt(j)<='z'  || s.charAt(i)>='0'&& s.charAt(i)<='9')
-             rev += s.charAt(j);
-             else  if(s.charAt(j)>='A' && s.charAt(j)<='Z')
-             rev += (char)((int)s.charAt(j)+32);
-             
+            i++;
+            continue;
         }
-        return new1.equals(rev);
+        else if (s.charAt(i)<='Z'&& s.charAt(i)>='A'){
+            I+=32;
+        }
+        if((s.charAt(j)>'Z'|| s.charAt(j)<'A') && (s.charAt(j)>'z'|| s.charAt(j)<'a') &&(s.charAt(j)<'0'|| s.charAt(j)>'9') ){
+            j--;
+            continue;
+        }
+         else if (s.charAt(j)<='Z'&& s.charAt(j)>='A'){
+            J+=32;
+        }
+    
+        if(I!=J){
+            x=false;
+            break;
+        }
+        
+         i++;j--;
+       }
+       return x;
     }
 }
