526db2280dfc7ef4db5f81a54e56cf13bb86d1a7
Tarun <gtarun.cs25@bmsce.ac.in>
2026-08-31 08:15:43 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 44.8 MB (86.72%)

--- 0082-remove-duplicates-from-sorted-list-ii/solution.java
+++ 0082-remove-duplicates-from-sorted-list-ii/solution.java
@@ -8,7 +8,7 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
+class Solution {//6
     public ListNode deleteDuplicates(ListNode head) {
         if (head == null || head.next == null){
             return head;
