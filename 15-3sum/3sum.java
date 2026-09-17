8e78fd73a7c29e0864469887ab72fe235357d7ed
G Tarun <gtarun.cs25@bmsce.ac.in>
2026-09-17 14:43:18 +0530

    Time: 35 ms (40.99%) | Memory: 58.9 MB (85.84%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,7 +1,6 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> x = new ArrayList<>();
-        List<Integer> y = new ArrayList<>();
         Arrays.sort(nums);
         
       for (int i = 0;i<nums.length - 2 ; i ++){
@@ -22,7 +21,7 @@ class Solution {
              x.add(Arrays.asList(nums[i], nums[j], nums[k]));
 
                     j++;
-                    while (nums[j] == nums[j-1] && j<k){
+                    while ( j<k && nums[j] == nums[j-1] ){
                         j++;
                     }
                     k--;
